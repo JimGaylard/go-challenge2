7988662b464eb00290e475c199148eae40efc22c
Jim Gaylard <jgaylard75@gmail.com>
2015-04-02 11:06:49 +0000

    Implemented SecureWriter

--- main.go
+++ main.go
@@ -1,22 +1,92 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+
+	"golang.org/x/crypto/nacl/box"
 )
 
-// NewSecureReader instantiates a new SecureReader
-func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
-	return nil
+type secureWriter struct {
+	Writer io.Writer
+	Pub    *[32]byte
+	Priv   *[32]byte
+}
+
+func (w *secureWriter) Write(p []byte) (n int, err error) {
+	nonce, err := newNonce()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := make([]byte, 32)
+
+	encrypted := box.Seal(out, p, nonce, w.Pub, w.Priv)
+
+	n, err = w.Writer.Write(encrypted)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return n, err
 }
 
 // NewSecureWriter instantiates a new SecureWriter
 func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
-	return nil
+	sw := &secureWriter{
+		Writer: w,
+		Pub:    pub,
+		Priv:   priv,
+	}
+
+	return sw
+}
+
+type secureReader struct {
+	reader io.Reader
+	pub    *[32]byte
+	priv   *[32]byte
+}
+
+func (r *secureReader) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+// NewSecureReader instantiates a new SecureReader
+func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
+	msg, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nonce, err := newNonce()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out := make([]byte, 1024)
+
+	decrypted, done := box.Open(out, msg, nonce, pub, priv)
+	if done != true {
+		log.Fatal("box.Open returned 'false'")
+	}
+	return bytes.NewReader(decrypted)
+}
+
+func newNonce() (*[24]byte, error) {
+	var nonce [24]byte
+	slice := nonce[:]
+
+	if _, err := rand.Read(slice); err != nil {
+		return nil, err
+	}
+	return &nonce, nil
 }
 
 // Dial generates a private/public key pair,
